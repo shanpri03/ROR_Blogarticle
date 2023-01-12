fabcb86987553957291d786d4c966bf9371f515a
= <spriyasathish03@gmail.com>
2023-01-12 09:49:39 +0530

    Updated with Postman request

--- app/controllers/blogarticle_controller.rb
+++ app/controllers/blogarticle_controller.rb
@@ -1,6 +1,7 @@
 class BlogarticleController < ApplicationController
+    skip_before_action :verify_authenticity_token
     def index
-        render json:Blgarticle.all
+        render json: Blgarticle.all
     end
 
     def show
@@ -8,9 +9,35 @@ class BlogarticleController < ApplicationController
         b = Blgarticle.exists?(params[:id])
         if b
         v = Blgarticle.find(params[:id])
-        render json:
+        render json: v
         else
         render html:"No data found"     
        end
-    end 
+
+    end
+    
+    def create
+       v=!params[:atitle].empty?
+       if(v)
+        Blgarticle.create('atitle': params[:atitle], 'acontent': params[:acontent], 'aimg': params[:aimg])
+        puts p
+        render json: "Data added"
+       else
+        render json: "Data not added"
+         end
+    
+    end
+
+    def update
+        b = Blgarticle.find(params[:id].to_i)
+        b.update('atitle': params[:atitle], 'acontent': params[:acontent], 'aimg': params[:aimg])
+        render json: "Data Updated"
+    end
+
+    def destroy
+        a = Blgarticle.find(params[:id].to_i)
+        a.destroy
+        render json: "Data deleted"
+    end
+
 end
